59c1b7fe709c57437a0466f3e45064a811e7d6bd
mac 2018 <mac2018@macs-MacBook-Pro.local>
2020-08-22 11:28:07 +0700

    version 0.1.1

---
+++ index.js
@@ -0,0 +1,5 @@
+import {requireNativeComponent, View} from 'react-native';
+import React from 'react';
+
+const AvatarHexagon = requireNativeComponent('AvatarHexagon', null);
+export default AvatarHexagon;
